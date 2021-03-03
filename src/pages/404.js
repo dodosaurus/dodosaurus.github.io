4debd6cc4f1e5b2003dbf11860e96e695fe72f1b
dodosaurus <kovac.jozef25@gmail.com>
2021-03-03 17:38:52 +0100

    inital commit

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const NotFound = () => {
+    return (
+        <Layout>
+            <section>
+                <h3>404 - Not found</h3>
+            </section>
+        </Layout>
+    )
+}
+
+export default NotFound
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
 import React from "react"
+import Layout from "../components/Layout"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Home = () => {
+  return (
+    <Layout>
+      <section className="m-5">
+        <h3>Welcome to JK Gatsby Playground</h3>
+        <p>Skuska paragrafu v gatsbyovi</p>
+        <figure className="m-3 border-black">
+          <img src="/pexels-rodolfo-clix-1036936.jpg" alt="Lightbulbs on strings"/>
+        </figure>
+      </section>
+    </Layout>
+  )
 }
+
+export default Home
